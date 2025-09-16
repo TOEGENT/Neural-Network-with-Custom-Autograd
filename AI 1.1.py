0839e76f117652660caa31500c8752e4bc5f6971
TOEGENT <119882568+TOEGENT@users.noreply.github.com>
2025-09-16 10:42:03 +0500

    Update AI 1.1.py
    новая архитектура (пока нерабочая)

--- AI 1.1.py
+++ AI 1.1.py
@@ -1,7 +1,174 @@
 import numpy as np
-
+from math import sin
 np.random.seed(4245)
 
+class NeuralNet:
+    def __init__(self,xs,arch,metric,target_func,act,der_act):
+
+        """
+        Инициализация весов и bias для сети с заданной архитектурой.
+
+        - Веса инициализируются случайно из U(-1, 1).
+        - Bias инициализируются нулями.
+
+        Преобразует "плоский" список весов и bias в структурированное представление,
+        чтобы удобно работать при прямом проходе.
+
+        architecture = [кол-во входов, нейронов в слое 1, нейронов в слое 2, ..., выходов]
+
+        Пример:
+            architecture = [2, 3, 2]
+            ws (длина 6) -> [[w11,w12],[w21,w22],[w31,w32]]
+            bs (длина 3) -> [[b1,b2,b3]]
+
+        Возвращает:
+            (structured_ws, structured_bs)
+            structured_ws: список слоёв, где каждый слой = список нейронов,
+                           а каждый нейрон = список его весов
+            structured_bs: список слоёв, где каждый слой = список bias
+        """
+        self.arch = arch
+        self.target_func = target_func
+        self.metric = metric
+        self.act = act
+        self.der_act = der_act
+
+        self.xs = xs
+        self.xs_min = min(self.xs)
+        self.xs_max = max(self.xs)
+        self.ys = [self.target_func(x) for x in self.xs]
+        self.ys_min = min(self.ys)
+        self.ys_max = max(self.ys)
+
+        self.ws_per_layer = [self.arch[i] * self.arch[i + 1] for i in range(len(self.arch) - 1)]
+        self.ws = []
+        for _ in range(sum(self.ws_per_layer)):
+            self.ws.append(Param(np.random.uniform(-1, 1), 1))  # raw_params=1, т.к. это обучаемые параметры
+        self.bs = []
+        for b_num in self.arch[1:]:
+            for _ in range(b_num):
+                self.bs.append(Param(0, 1))  # bias инициализируем нулями
+        self.structurise_ws_bs()
+
+    def generate_normalized_xs(self,additional=None):
+
+        result = []
+        if not additional:
+            for xi in range(len(self.xs)):
+                result.append((self.xs[xi] - self.xs_min) / (self.xs_max - self.xs_min))
+        if additional:
+            for xi in range(len(additional)):
+                result.append((additional[xi] - self.xs_min) / (self.xs_max - self.xs_min))
+        return result
+
+    def generate_denormalized_predicted(self):
+        result = []
+        for xi in range(len(self.current_xs)):
+            result.append(self.current_xs[xi] * (self.ys_max - self.ys_min) + self.ys_min)
+        return result
+
+
+
+    def forward(self, xs,grad=True,norm=True):
+        """
+        Прямое распространение сигнала (forward pass) с вычислением градиентов.
+        xs: входной вектор (список чисел или Param).
+        ws: структурированные веса.
+        bs: структурированные bias.
+
+        На каждом слое:
+            y = act(sum(w_i * x_i) + b)
+
+        Возвращает:
+            список выходных Param текущего слоя (последний слой = выход сети).
+        """
+        if norm:
+            self.current_xs = self.generate_normalized_xs([xs])
+        else:
+            self.current_xs = xs
+
+        for i in range(len(self.structured_ws)):  # по каждому слою
+            new_xs = []
+            for m in range(len(self.structured_ws[i])):  # по каждому нейрону в слое
+                # линейная комбинация входов + bias → активация
+                if grad:
+                    new_xs.append(act(sum([self.structured_ws[i][m][xi] * self.current_xs[xi] for xi in range(len(self.structured_ws[i][m]))]) + self.structured_bs[i][m]))
+                elif isinstance(self.current_xs[xi],Param):
+                    new_xs.append(act(sum([self.structured_ws[i][m][xi].value * self.current_xs[xi].value for xi in range(len(self.structured_ws[i][m]))]) + self.structured_bs[i][m]))
+                else:
+                    new_xs.append(act(sum([self.structured_ws[i][m][xi].value * self.current_xs[xi] for xi in range(len(self.structured_ws[i][m]))]) + self.structured_bs[i][m]))
+
+            self.current_xs = new_xs  # выход слоя становится входом для следующего
+        if norm and not grad:
+            self.current_xs = self.generate_denormalized_predicted(self.current_xs)
+        return self.current_xs
+    def structurise_ws_bs(self,ws=0, bs=0):
+        if ws+bs==0:
+            ws=self.ws
+            bs=self.bs
+        """
+        Преобразует "плоский" список весов и bias в структурированное представление,
+        чтобы удобно работать при прямом проходе.
+
+        architecture = [кол-во входов, нейронов в слое 1, нейронов в слое 2, ..., выходов]
+
+        Пример:
+            architecture = [2, 3, 2]
+            ws (длина 6) -> [[w11,w12],[w21,w22],[w31,w32]]
+            bs (длина 3) -> [[b1,b2,b3]]
+
+        Возвращает:
+            (structured_ws, structured_bs)
+            structured_ws: список слоёв, где каждый слой = список нейронов,
+                           а каждый нейрон = список его весов
+            structured_bs: список слоёв, где каждый слой = список bias
+        """
+        self.structured_ws = [[] for _ in range(len(self.arch) - 1)]
+        self.arch_struct = [[self.arch[i], self.arch[i + 1]] for i in range(len(self.arch) - 1)]
+        start = 0
+        for i in range(len(self.arch_struct)):
+            slice = ws[start:start + self.ws_per_layer[i]]
+            # Разбиваем веса для каждого нейрона слоя
+            self.structured_ws[i] = [slice[j:j + self.arch_struct[i][0]] for j in
+                                     range(0, len(slice), self.arch_struct[i][0])]
+            start += self.ws_per_layer[i]
+        step = 0
+        self.structured_bs = []
+        for b_num in self.arch[1:]:  # bias только для скрытых и выходных слоёв
+            slice = bs[step: b_num + step]
+            self.structured_bs.append(slice)
+            step += b_num
+
+        if ws+bs!=0:
+            return (ws, bs)
+    def train(self,epochs,lr):
+        self.lr = lr
+        self.epochs = epochs
+        self.nxs = self.generate_normalized_xs()
+        all_iterations_data = []
+        for epoch in range(self.epochs):
+            total_loss = 0
+            for xindex in range(len(self.xs)):
+                # Прямой проход
+                y_pred = self.forward(self.nxs[xindex])[0]
+                y_true = self.target_func(self.nxs[xindex])
+                # Функция ошибки (MSE)
+                loss=self.metric(y_pred,y_true)
+                total_loss += loss.value
+
+                # Обновление весов и bias через градиентный спуск
+                for w in self.ws:
+                    grad = w.chain[list(w.chain.keys())[-1]]
+                    w.value -= self.lr * grad
+                    w.chain = {w: 1}
+                    w.history = {w}
+                for b in self.bs:
+                    grad = b.chain[list(b.chain.keys())[-1]]
+
+                    b.value -= self.lr* grad
+                    b.chain = {b: 1}
+                    b.history = {b}
+        return {"ws":self.ws.copy(),"bs":self.bs.copy(),"structured":self.structurise(self.ws.copy(),self.bs.copy())}
 
 class Param:
     """
@@ -128,7 +295,6 @@ class Param:
 
     def __neg__(self):
         return self * (-1)
-
 
 def der_act(act, alpha=0.01):
     """ Производная активации ReLU/LeakyReLU. """
@@ -281,10 +447,20 @@ def denormalize_data(data,data_min,data_max):
     for xi in range(len(data)):
         result.append(data[xi]*(data_max-data_min)+data_min)
     return result
-# Подготовка данных
+
+def MSE(predicted,expected):
+    return (expected-predicted)**2
 r = [i for i in range(-10, 11)]  # входы
+
+NN = NeuralNet(r,[1,8,1],MSE,lambda x:x**2,act,der_act)
+
+
+NN.train(2000,0.01)
+
+target_func = lambda x:x**2
+# Подготовка данных
 XS = [i for i in r]
-YS = [(i)**3 for i in r]  # целевая функция y = x^3
+YS = [target_func(i) for i in r]  # целевая функция y = x^3
 
 # Нормализация данных
 xs_min, xs_max = min(XS), max(XS)
@@ -299,14 +475,14 @@ ys = [[y] for y in YS]
 xs = [[x] for x in XS]
 
 # Архитектура сети: [1 вход → 6 скрытых нейронов → 1 выход]
-arch = [1, 8,8, 1]
-epochs = 500
-lr_w = 0.1
-lr_b = 0.1
+arch = [1, 8, 1]
+epochs = 2000
+lr_w = 0.01
+lr_b = 0.01
 
 all_iterations_data = []
 
-for I in range(15):  # несколько запусков обучения
+for I in range(10):  # замени на NN
     ws, bs = init_params(arch)  # инициализация
     structured_ws, structured_bs = structurise(ws, bs, arch)
 
@@ -339,9 +515,15 @@ for I in range(15):  # несколько запусков обучения
     structured_well = structurise(well_done_ws, well_done_bs, arch)
 
     forward_xs = []
-    for i in r:
-        forward_xs.append(forward_no_grad([i], structured_well[0], structured_well[1], xs_min, xs_max, ys_min, ys_max,True))
+    r2 = range(-20,20)
+    y_r2=[target_func(x) for x in r2]
+    total_loss_r2=0
+    for i in r2:
 
+        forward_xs.append(forward_no_grad([i], structured_well[0], structured_well[1], xs_min,xs_max, ys_min, ys_max,True))
+        loss = (y_true - y_pred) ** 2
+        total_loss_r2 += loss.value
+        total_loss_r2+=forward_xs[-1][0]
     print(I, "loss", total_loss)
 
     all_iterations_data.append({
@@ -361,7 +543,7 @@ def plot_combined_overlaid(data_list, x_range):
     """
     plt.figure(figsize=(12, 6))
 
-    true_y = [x**3 for x in x_range]
+    true_y = [target_func(x) for x in x_range]
     plt.plot(x_range, true_y, 'k--', linewidth=2, label='y = x^3 (true)')
 
     for data in data_list:
@@ -376,4 +558,4 @@ def plot_combined_overlaid(data_list, x_range):
     plt.tight_layout()
     plt.show()
 
-plot_combined_overlaid(all_iterations_data, r)
+plot_combined_overlaid(all_iterations_data, r2)
